d3074f5f872794bd3ff4de0d6f048cce32846340
Lindong Zhao <lindong.zhao@nokia-sbell.com>
2022-01-11 09:53:12 +0800

    init spring boot project

---
+++ consumer/src/main/java/com/restdemo/consumer/ConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.restdemo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lindzhao
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
